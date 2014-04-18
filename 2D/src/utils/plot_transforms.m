3d9903cd21f5c086117d6c238c3881e46782c0f3
MichaelF <mdfirman@gmail.com>
2014-04-18 11:25:30 +0100

    Nicer plotting of transformed basis shapes

--- 2D/src/utils/plot_transforms.m
+++ 2D/src/utils/plot_transforms.m
@@ -2,25 +2,54 @@ function plot_transforms(transformed, output_img, gt_img)
 
 max_height = 200;
 
+% settiping up subplot dimensions
+plot_n = 2;
+plot_m = 5;
+
+% rendering the depth of the gt image
 gt_depth = raytrace_2d(gt_img);
 gt_y = 1:length(gt_depth);
 
-subplot(4, 5, 1); 
+% plotting the ground truth mask and the rendered depth
+subplot(plot_n, plot_m, 1); 
 imagesc(gt_img(1:max_height, :))
-axis image
+hold on
+plot(gt_y, gt_depth, 'c', 'LineWidth', 1.5)
+hold off
+axis image off
+title('Input (blue) + GT image')
 
-for ii = 1:min(18, length(transformed)); 
-    subplot(4, 5, ii+1); 
-    imagesc(transformed(ii).extended_mask(1:max_height, :)); 
-    axis image
+% deciding which basis shapes to use
+to_use = 1:min(8, length(transformed));
+
+for jj = 1:length(to_use)
+    
+    ii = to_use(jj);
+    
+    % plotting the basis shape
+    subplot(plot_n, plot_m, jj+1); 
+    imagesc(transformed(ii).cropped_mask(1:max_height, :)); 
+    colormap(flipgray)
+    axis image off
+    
+    % plotting the rendered depth from the basis shape
     hold on
     X = transformed(ii).transformed_depth(1, :) - transformed(ii).padding;
     Y = transformed(ii).transformed_depth(2, :) - transformed(ii).padding;
-    plot(X, Y, 'g')
-    plot(gt_y, gt_depth, 'c')
+    plot(X, Y, 'g', 'LineWidth', 1.5)
+    
+    % plotting the input depth
+    plot(gt_y, gt_depth, 'c', 'LineWidth', 1.5)
+    
+    title(['B_' num2str(jj)])
     hold off
 end
 
-subplot(4, 5, 20); 
+% plotting the aggregated basis shapes
+subplot(plot_n, plot_m, 10); 
 imagesc(output_img(1:max_height, :))
-axis image
+axis image off
+title('Combination of basis shapes')
+
+% making the font larger for the whole figure
+set(findall(gcf,'type','text'),'fontSize',14)
