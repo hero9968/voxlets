563c41dfba6eedf7bc5618e91f2a92260087974f
MichaelF <mdfirman@gmail.com>
2014-04-23 14:28:13 +0100

    Changed segment_and_normals to use the new structure-based system

--- 2D/segment_and_normals.m
+++ 2D/segment_and_normals.m
@@ -5,6 +5,7 @@
 % might be
 
 clear
+cd ~/projects/shape_sharing/2D
 define_params
 load(paths.filelist)
 addpath src/utils
@@ -25,19 +26,23 @@ for ii = 1:number_shapes
     readname = sprintf(paths.raytraced_savename, ii);
     load(readname, 'rotated');
     
-    for jj = 1:length(rotated.raytraced)
+    for jj = 1:length(rotated.rendered)
         
-        this_depth = rotated.raytraced{jj};
+        this_depth = rotated.rendered(jj).depth;
         
         % segmenting the depth into a soup
-        rotated.segmented{jj} = ...
+        rotated.rendered(jj).segmented = ...
             segment_soup_2d(this_depth, params.segment_soup);
         
         % computing normals from the depth
         XY = [1:length(this_depth); this_depth];
         
-        rotated.normals{jj} = ...
-            normals_radius_2d(XY, params.normal_radius);
+        rotated.rendered(jj).normals = ...
+            single(normals_radius_2d(XY, params.normal_radius));
+        
+        % converting depth to single for space reasons
+        rotated.rendered(jj).depth = single(rotated.rendered(jj).depth);
+        
     end
     
     % saving the whole structure back to the original location
