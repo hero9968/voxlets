2cffa4add5545f624dbd8970a69dadeff74d8f63
MichaelF <mdfirman@gmail.com>
2014-04-23 13:38:45 +0100

    Got rotating working in a separate function, which returns a structure of image, transformations and raytraced images

--- 2D/rotate_and_raytrace.m
+++ 2D/rotate_and_raytrace.m
@@ -17,74 +17,22 @@ if ~exist(paths.raytraced, 'dir')
 end
 
 %%
-%profile on
+profile on
 
 
-for ii = 500%:length(filelist)
+for ii = 1%:length(filelist)
     
     clear rotated
     
-    rotated.idx = ii;
-    rotated.filename = filelist(ii).filename;
-    
     % load in this image, and ensure if uint8 in [0, 255]
     this_path = [paths.mpeg,  filelist(ii).filename];
-    rotated.original_image = uint8(imread(this_path));
-    rotated.original_image(rotated.original_image > 0) = 255;
+    img_in = uint8(imread(this_path));
+    img_in(img_in > 0) = 255;
     
     % crop edges from image
-    rotated.image = boxcrop_2d(rotated.original_image);
-    
-    % resize image according to predefined scale
-    rotated.image = imresize(rotated.image, params.scale);
-    
-    % getting image height and width
-    imheight = size(rotated.image, 1);
-    imwidth = size(rotated.image, 2);
-    
-    % get the diagonal size of the image
-    diag_size = sqrt(imheight^2 + imwidth^2);
-    
-    rotated.angles = params.angles;
-    
-    % loop over each possible rotation and 
-    for jj = 1:length(rotated.angles)
-        
-        % develop the transformation matrix
-        T_translate_1 = translation_matrix(-imwidth/2, -imheight/2);
-        T_rotate = rotation_matrix(params.angles(jj));
-        T_translate_2 = translation_matrix(2 + diag_size/2, 2 + diag_size/2);
-        % ^^ adding 2 to diag size to allow for rounding errors etc.
-        
-        % combining and converting to MATLAB format
-        T_final = T_translate_2 * T_rotate * T_translate_1;
-        T_final_M = maketform('affine', T_final');
-        
-        % applying transformation
-        this_rotated_image = imtransform((rotated.image), T_final_M, 'bilinear',...
-            'xdata', [1, diag_size + 2], ...
-            'ydata', [1, diag_size + 2]);
-        
-        % want to give function the image and the angle, and get back the
-        % rotated image and a transformation which brings it back in line
-        % with the origin
-        
-        % raytracing this rotated image
-        rotated.raytraced{jj} = raytrace_2d(this_rotated_image > 128);                
-        
-        % plot this image
-        if plotting
-            [n, m] = best_subplot_dims(length(rotated.angles));
-            subplot(n, m, jj)
-            imagesc(this_rotated_image)
-            axis image
-            colormap(flipgray)
-            hold on
-            plot(1:length(rotated.raytraced{jj}), rotated.raytraced{jj}, 'linewidth', 3);
-            hold off
-        end
-        
-    end
+    rotated = rotate_and_raytrace_mask(img_in, params.angles, params.scale);
+    rotated.idx = ii;
+    rotated.filename = filelist(ii).filename;
     
     % save this rotated image
     if saving
@@ -94,5 +42,5 @@ for ii = 500%:length(filelist)
     
     done(ii, length(filelist))
 end
-%profile off viewer
+profile off viewer
 
---
+++ 2D/src/utils/rotate_and_raytrace_mask.m
@@ -0,0 +1,62 @@
+function [rotated] = rotate_and_raytrace_mask(mask, angles, scale)
+% takes a binary mask and a set of angles in degrees
+% returns a structure containing:
+% - the original iamge
+% - a resized and cropped image
+% - a set of transformations to rotate and translate the cropped image
+% - a set of raytraces of the image
+
+plotting  = 0;%params.plotting.plot_rotated_masks;
+
+% crop edges from image
+rotated.original_image = mask;
+rotated.image = boxcrop_2d(mask);
+
+% resize image according to predefined scale
+rotated.image = imresize(rotated.image, scale);
+
+% getting image height and width
+imheight = size(rotated.image, 1);
+imwidth = size(rotated.image, 2);
+
+% get the diagonal size of the image
+diag_size = sqrt(imheight^2 + imwidth^2);
+
+rotated.angles = angles;
+
+% loop over each possible rotation and 
+for jj = 1:length(rotated.angles)
+
+    % develop the transformation matrix
+    T_translate_1 = translation_matrix(-imwidth/2, -imheight/2);
+    T_rotate = rotation_matrix(rotated.angles(jj));
+    T_translate_2 = translation_matrix(2 + diag_size/2, 2 + diag_size/2);
+    % ^^ adding 2 to diag size to allow for rounding errors etc.
+
+    % combining and converting to MATLAB format
+    T_final = T_translate_2 * T_rotate * T_translate_1;
+    rotated.transform(jj) = maketform('affine', T_final');
+
+    % applying transformation
+    this_rotated_image = ...
+        imtransform(rotated.image, rotated.transform(jj), ...
+        'bilinear', ...
+        'xdata', [1, diag_size + 2], ...
+        'ydata', [1, diag_size + 2]);
+
+    % raytracing this rotated image
+    rotated.raytraced{jj} = raytrace_2d(this_rotated_image > 128);                
+
+    % plot this image
+    if plotting
+        [n, m] = best_subplot_dims(length(rotated.angles));
+        subplot(n, m, jj)
+        imagesc(this_rotated_image)
+        axis image
+        colormap(flipgray)
+        hold on
+        plot(1:length(rotated.raytraced{jj}), rotated.raytraced{jj}, 'linewidth', 3);
+        hold off
+    end
+
+end
