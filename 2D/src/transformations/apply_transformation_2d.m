beb59339411c465207945650a0baf4b131081ccb
MichaelF <mdfirman@gmail.com>
2014-04-28 11:31:30 +0100

    Transformation can now be either projective or affine. Helper functions for homogeneous conversions

--- 2D/src/transformations/apply_transformation_2d.m
+++ 2D/src/transformations/apply_transformation_2d.m
@@ -1,19 +1,34 @@
-function XY_out = apply_transformation_2d(XY, transformation)
+function XY_out = apply_transformation_2d(XY, transformation, transform_type)
 % apply a 3x3 transformation matrix to 2d points
-% no longer using homogeneous coordinates as this way is much quicker
+% affine way for speed ? projective way for full transform
 
 % input check
 assert(size(XY, 1)==2);
 
-% extracting rotation and translation parts from matrix
-T_rot = transformation(1:2, 1:2);
-T_trans = transformation(1:2, 3);
+if strcmp(transform_type, 'affine')
+    
+    % extracting rotation and translation parts from matrix
+    T_rot = transformation(1:2, 1:2);
+    T_trans = transformation(1:2, 3);
 
-% apply rotation
-XY_rot = T_rot * XY;
+    % apply rotation
+    XY_rot = T_rot * XY;
 
-% apply transformation
-XY_out = [XY_rot(1, :) + T_trans(1); XY_rot(2, :) + T_trans(2)];
+    % apply transformation
+    XY_out = [XY_rot(1, :) + T_trans(1); XY_rot(2, :) + T_trans(2)];
+    
+elseif strcmp(transformation_type, 'projective')
+
+    % convert to homogeneous coordinates
+    XY_hom = cart2hom(XY);
+
+    % apply transformation
+    XY_trans = (transformation * XY_hom);
+
+    % convert back to cartesian
+    XY_out = hom2cart(XY_trans);
+
+end
 
 % output check
 assert(all(size(XY_out)==size(XY)));
