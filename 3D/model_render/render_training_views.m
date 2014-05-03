d08747956501d133041460adc3606f72d71bf8bb
MichaelF <mdfirman@gmail.com>
2014-05-03 12:07:23 +0100

    Made rendering a bit nicer with some additions for not bothering if file exists

--- 3D/model_render/render_training_views.m
+++ 3D/model_render/render_training_views.m
@@ -1,27 +1,62 @@
 % a script to render the models from different angles, using the python
 % script.
 %
+cd ~/projects/shape_sharing/3D/model_render/
+clear
+run ../define_params_3d.m
 
-for ii = 1:length(ply_consts.model_names)
+%% generating a reduced filelist which only consists of files which are small enough
+% (This has now been incorporated into the main model list)
+%{
+fid = fopen('temp.txt', 'w');
+
+for ii = 1:length(params.model_filelist)
+    P = dir(fullfile(paths.basis_models.centred, [params.model_filelist{ii} '.obj']));
+    
+    if P.bytes < 1e6
+        fprintf(fid, '%s\n', params.model_filelist{ii});
+    end
+end
+
+fclose(fid);
+%}
+
+%%
+for ii = 1:length(params.model_filelist)
+    
+    
     
     model = params.model_filelist{ii};
     
     % loading meta file
     meta_file = [paths.basis_models.centred '/' params.model_filelist{ii} '.mat'];
     load(meta_file, 'aabb');
-    distance = aabb.diag / ( 2 * tand(43/2) ) % (vertical FOV is 43 degrees)
+    %distance = aabb.diag / ( 2 * tand(43/2) ) % (vertical FOV is 43 degrees)
+    distance = 1; % as all shapes should be nice and rescaled
     
     % creating output directory if it doesn't exist
     outdir = ['/Users/Michael/projects/shape_sharing/data/3D/basis_models/renders/' model];
+    
     if ~exist(outdir, 'dir')
-        mkdir outdir
+        disp(['Making directory']);
+        mkdir(outdir)
     end
     
-    % calling python script to render the sequence
-    system_call = ['python getDepthSequence.py ' model ' ' num2str(1.1*distance) ' 1 42'];
-    %[A, B] = system(system_call);
+	% deciding if to continue
+    if length(dir(outdir)) >= 44
+        disp(['Skipping number ' num2str(ii)]);
+        continue;
+    end
     
-    ii
+    disp(['Doing number ' num2str(ii)]);
+    
+    % calling python script to render the sequence
+    system_call = ['python src/getDepthSequence.py ' model ' ' num2str(1.5*distance) ' 1 42'];
+    %system_call
+    [A, B] = system(system_call);
+    pause(1)
+    
+    disp(['Done ' num2str(ii)]);
 end
 
 
