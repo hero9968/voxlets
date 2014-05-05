efc6631ce90f78f46a6994004d64819e2d7f7c5a
MichaelF <mdfirman@gmail.com>
2014-05-05 14:12:06 +0100

    Full saving of mat files. Removed some old shit from the code

--- 3D/generate_halo.m
+++ 3D/generate_halo.m
@@ -1,15 +1,20 @@
 %% script to generate halo and probably save files to disk...
 clear
+cd ~/projects/shape_sharing/3D
 run define_params_3d.m
 close all
+addpath model_render/src/
+addpath plotting/
 plotting = 1;
-hemisphere = 0;
 
 if ~exist(paths.basis_models.halo_path, 'dir')
     mkdir(paths.basis_models.halo_path)
 end
 
 %%
+
+% setting the intrinsic matrix (just for the saving)
+K = params.half_intrinsics;
 
 % setting parameters for the halo creation
 level = params.model_render.level;
@@ -62,85 +67,12 @@ for ii = 1:size(xyz, 1)
     % saving transformation
     csv_name = sprintf(paths.basis_models.halo_file, ii);
     csvwrite(csv_name, H);
+    
+    R = H';
+    mat_name = sprintf(paths.basis_models.halo_file_mat, ii);
+    save(mat_name, 'R', 'K')
     % done(ii)
     ii
     
 end
 
-
-%% plotting the depth images created
-close all
-clear temp
-for ii = 1:16
-    filename = sprintf('/Users/Michael/Data/Others_data/google_warehouse/obj_beds/BED/depths/depth_%d.mat', ii);
-    
-    temp(ii) = load(filename);
-    temp(ii).depth( abs(temp(ii).depth - 10) < 0.001 ) = nan;
-    
-    
-    subplot(4, 4, ii);
-    imagesc2(temp(ii).depth);
-    axis on
-    
-end
-
-
-%% now want to try to get the point clouds out and realign using the matrices I have...
-close all
-clear temp
-cols = 'ymcrgbwkymcrgbwkymcrgbwkymcrgbwkymcrgbwk';
-
-for ii = 1:16
-    
-    filename = sprintf('/Users/Michael/Data/Others_data/google_warehouse/obj_beds/BED/depths/depth_%d.mat', ii);
-    
-    % loading depth
-    T = load(filename);
-    temp(ii).depth = T.depth;
-    temp(ii).depth( abs(temp(ii).depth - 10) < 0.001 ) = nan;
-
-    % converting to XYZ
-    temp(ii).xyz = 1000*depth2xyz(temp(ii).depth);
-    
-    % now transforming using the transformation
-    %trans = pinv(all_transforms(:, :, ii));
-    %trans(2, 4) = -trans(2, 4);
-    %trans = eye(4);
-    if 1
-        
-        trans = pinv(all_transforms(:, :, ii))
-        %trans(1:3, 3) = -trans(1:3, 3)
-        %RR = (trans(1:3, 1:3))';
-        %RR = (all_transforms(1:3, 1:3, ii));
-        RR_inv = trans(1:3, 1:3);
-        %tttemp = RR_inv(:, 3);
-        %RR_inv(:, 3) = RR_inv(:, 2);
-        %RR_inv(:, 2) = tttemp;
-        
-        TT = trans(1:3, 4);
-
-        temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz, TT);
-        temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz_trans, RR_inv);
-        %temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz_trans, RR);
-        %temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz, pinv(RR));
-        %temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz_trans, pinv(trans));
-        
-        %temp(ii).xyz_trans = apply_transformation_3d(temp(ii).xyz_trans,);
-        
-    else
-        temp(ii).xyz_trans = temp(ii).xyz;
-    end
-    %subplot(4, 4, ii);
-    %imagesc2(temp(ii).depth);
-    plot3d(temp(ii).xyz_trans, cols(ii));
-    hold on
-end
-hold off
-
-
-
-
-%D = reshape(depth', [480, 640]);
-%D = depth;
-%imagesc2(T.depth)
-%whos d*
