c6c557679a05796e00e90b12a0095e53933c802b
MichaelF <mdfirman@gmail.com>
2014-05-19 13:08:24 +0100

    Minor edits to allow arbitrary number of transforms to be plotted

--- 2D/src/predict/plot_transforms.m
+++ 2D/src/predict/plot_transforms.m
@@ -1,10 +1,10 @@
 function plot_transforms(transformed, output_img, gt_img)
 
-max_height = 200;
+%max_height = 200;
 
 % settiping up subplot dimensions
-plot_n = 2;
-plot_m = 5;
+plot_n = 10;
+plot_m = 10;
 
 % rendering the depth of the gt image
 gt_depth = raytrace_2d(gt_img);
@@ -12,7 +12,7 @@ gt_y = 1:length(gt_depth);
 
 % plotting the ground truth mask and the rendered depth
 subplot(plot_n, plot_m, 1); 
-imagesc(gt_img(1:max_height, :))
+imagesc(gt_img)
 hold on
 plot(gt_y, gt_depth, 'c', 'LineWidth', 1.5)
 hold off
@@ -20,7 +20,7 @@ axis image off
 title('Input (blue) + GT image')
 
 % deciding which basis shapes to use
-to_use = 1:min(8, length(transformed));
+to_use = 1:min(plot_n*plot_m - 2, length(transformed));
 
 for jj = 1:length(to_use)
     
@@ -28,7 +28,7 @@ for jj = 1:length(to_use)
     
     % plotting the basis shape
     subplot(plot_n, plot_m, jj+1); 
-    imagesc(transformed(ii).cropped_mask(1:max_height, :)); 
+    imagesc(transformed(ii).cropped_mask); 
     colormap(flipgray)
     set(gca, 'clim', [0, 1])        
     axis image off
@@ -47,8 +47,8 @@ for jj = 1:length(to_use)
 end
 
 % plotting the aggregated basis shapes
-subplot(plot_n, plot_m, 10); 
-imagesc(output_img(1:max_height, :))
+subplot(plot_n, plot_m, plot_n*plot_m); 
+imagesc(output_img)
 axis image off
 title('Combination of basis shapes')
 
