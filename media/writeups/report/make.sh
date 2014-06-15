3fca3bdb24dec5647358f89cc3839b4f13848229
MichaelF <mdfirman@gmail.com>
2014-06-16 00:27:27 +0100

    Added lots fo bibliography. Images, of pipeline etc. Split many lines into separate senatances

--- media/writeups/report/make.sh
+++ media/writeups/report/make.sh
@@ -1,6 +1,6 @@
 # An example build process
 # mkdir build
-pdflatex -output-directory=build survey.tex
-bibtex -min-crossrefs=99 build/survey.aux
-pdflatex -output-directory=build survey.tex
-pdflatex -output-directory=build survey.tex
+pdflatex -output-directory=build writeup.tex
+bibtex -min-crossrefs=99 build/writeup.aux
+pdflatex -output-directory=build writeup.tex
+pdflatex -output-directory=build writeup.tex
