76017f09a0f16bd0d52ad14b20040779be2b0ec9
MichaelF <mdfirman@gmail.com>
2014-07-11 13:44:57 +0100

    Prism segmentation working. Recognition probably not working at the moment...

--- src/pcl_pipeline/recogniser/recognise_global.cpp
+++ src/pcl_pipeline/recogniser/recognise_global.cpp
@@ -6,128 +6,130 @@
  */
 
 #include <pcl/pcl_macros.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/apps/3d_rec_framework/pipeline/global_nn_classifier.h>
 #include <pcl/apps/3d_rec_framework/pc_source/mesh_source.h>
 #include <pcl/apps/3d_rec_framework/feature_wrapper/global/vfh_estimator.h>
 #include <pcl/apps/3d_rec_framework/feature_wrapper/global/esf_estimator.h>
 #include <pcl/apps/3d_rec_framework/feature_wrapper/global/cvfh_estimator.h>
-//#include <pcl/apps/3d_rec_framework/tools/openni_frame_source.h>
+#include <pcl/apps/3d_rec_framework/tools/openni_frame_source.h>
 #include <pcl/apps/3d_rec_framework/utils/metrics.h>
+#include <pcl/filters/filter.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/apps/dominant_plane_segmentation.h>
 #include <pcl/console/parse.h>
+//#include "my_prism_segment.cpp"
+
+std::pair<std::vector <pcl::PointCloud<pcl::PointXYZ>::Ptr>, 
+          std::vector <pcl::PointIndices> >
+myPrismSegmenter(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, 
+              pcl::PointCloud<pcl::Normal>::Ptr cloud_normals);
 
 template<template<class > class DistT, typename PointT, typename FeatureT>
 void
 segmentAndClassify (typename pcl::rec_3d_framework::GlobalNNPipeline<DistT, PointT, FeatureT> & global)
 {
-  //get point cloud from the kinect, segment it and classify it
-  OpenNIFrameSource::OpenNIFrameSource camera;
-  OpenNIFrameSource::PointCloudPtr frame;
 
+  // visualiser
   pcl::visualization::PCLVisualizer vis ("kinect");
-
-  //keyboard callback to stop getting frames and finalize application
-  boost::function<void
-  (const pcl::visualization::KeyboardEvent&)> keyboard_cb = boost::bind (&OpenNIFrameSource::OpenNIFrameSource::onKeyboardEvent, &camera, _1);
-  vis.registerKeyboardCallback (keyboard_cb);
+    
   size_t previous_cluster_size = 0;
   size_t previous_categories_size = 0;
 
   float Z_DIST_ = 1.25f;
   float text_scale = 0.015f;
 
-  while (camera.isActive ())
+  pcl::PointCloud<pcl::PointXYZ>::Ptr xyz_points (new pcl::PointCloud<pcl::PointXYZ>);
+  
+  // load point cloud
+  pcl::io::loadPCDFile<pcl::PointXYZ> ("../../data/scenes/scene2.pcd", *xyz_points);
+  //xyz_points->height = 480;
+  // xyz_points->width = 640;
+  // std::cout << "Size is " << xyz_points->size() << std::endl;
+  // std::cout << "Height is " << xyz_points->height << std::endl;
+  // std::cout << "Width is " << xyz_points->width << std::endl;
+  
+  std::vector< int > idx;
+  pcl::removeNaNFromPointCloud (*xyz_points, *xyz_points, idx);
+  vis.addPointCloud(xyz_points, "points");
+
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+  ne.setInputCloud (xyz_points);
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  ne.setSearchMethod (tree);
+  ne.setKSearch (50);
+  ne.compute (*cloud_normals);
+
+  //  pcl::copyPointCloud (*frame, *xyz_points);
+  std::cout << "Size is " << xyz_points->size() << std::endl;
+  std::cout << "Height is " << xyz_points->height << std::endl;
+  std::cout << "Width is " << xyz_points->width << std::endl;
+
+  //Step 1 -> Segment
+  std::pair<std::vector <pcl::PointCloud<pcl::PointXYZ>::Ptr>, 
+          std::vector <pcl::PointIndices> > cluster_out;
+  cluster_out = myPrismSegmenter(xyz_points, cloud_normals);
+
+  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters = cluster_out.first;
+  std::vector <pcl::PointIndices> indices = cluster_out.second;
+
+  std::cout << "Done the dps - found " << clusters.size() << " clusters " << std::endl;
+ // vis.removePointCloud ("frame");
+//  vis.addPointCloud<OpenNIFrameSource::PointT> (frame, "frame");
+
+
+  previous_categories_size = 0;
+  float dist_ = 0.03f;
+
+  // classifying each category
+  for (size_t i = 0; i < clusters.size (); i++)
   {
-    pcl::ScopeTime frame_process ("Global frame processing ------------- ");
-    frame = camera.snap ();
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr xyz_points (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::copyPointCloud (*frame, *xyz_points);
+    std::stringstream cluster_name;
+    cluster_name << "cluster_" << i;
+    pcl::visualization::PointCloudColorHandlerRandom<pcl::PointXYZ> random_handler (clusters[i]);
+    vis.addPointCloud<pcl::PointXYZ> (clusters[i], random_handler, cluster_name.str ());
 
-    //Step 1 -> Segment
-    pcl::apps::DominantPlaneSegmentation<pcl::PointXYZ> dps;
-    dps.setInputCloud (xyz_points);
-    dps.setMaxZBounds (Z_DIST_);
-    dps.setObjectMinHeight (0.005);
-    dps.setMinClusterSize (1000);
-    dps.setWSize (9);
-    dps.setDistanceBetweenClusters (0.1f);
+    global.setInputCloud (xyz_points);
+    global.setIndices (indices[i].indices);
+    global.classify ();
 
-    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clusters;
-    std::vector<pcl::PointIndices> indices;
-    dps.setDownsamplingSize (0.02f);
-    dps.compute_fast (clusters);
-    dps.getIndicesClusters (indices);
-    Eigen::Vector4f table_plane_;
-    Eigen::Vector3f normal_plane_ = Eigen::Vector3f (table_plane_[0], table_plane_[1], table_plane_[2]);
-    dps.getTableCoefficients (table_plane_);
+    std::vector < std::string > categories;
+    std::vector<float> conf;
 
-    vis.removePointCloud ("frame");
-    vis.addPointCloud<OpenNIFrameSource::PointT> (frame, "frame");
+    global.getCategory (categories);
+    global.getConfidence (conf);
 
-    for (size_t i = 0; i < previous_cluster_size; i++)
+    std::string category = categories[0];
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid (*xyz_points, indices[i].indices, centroid);
+    for (size_t kk = 0; kk < categories.size (); kk++)
     {
-      std::stringstream cluster_name;
-      cluster_name << "cluster_" << i;
-      vis.removePointCloud (cluster_name.str ());
 
-      cluster_name << "_ply_model_";
-      vis.removeShape (cluster_name.str ());
-    }
+      // deciding where to add the text string...
+      // pcl::PointXYZ pos;
+      // pos.x = centroid[0] + normal_plane_[0] * static_cast<float> (kk + 1) * dist_;
+      // pos.y = centroid[1] + normal_plane_[1] * static_cast<float> (kk + 1) * dist_;
+      // pos.z = centroid[2] + normal_plane_[2] * static_cast<float> (kk + 1) * dist_;
 
-    for (size_t i = 0; i < previous_categories_size; i++)
-    {
+      std::ostringstream prob_str;
+      prob_str.precision (1);
+      prob_str << categories[kk] << " [" << conf[kk] << "]";
+
       std::stringstream cluster_text;
-      cluster_text << "cluster_" << i << "_text";
-      vis.removeText3D (cluster_text.str ());
+      cluster_text << "cluster_" << previous_categories_size << "_text";
+      std::cout << "Found " << prob_str.str () << std::endl;
+      previous_categories_size++;
     }
+  }
 
-    previous_categories_size = 0;
-    float dist_ = 0.03f;
 
-    for (size_t i = 0; i < clusters.size (); i++)
-    {
-      std::stringstream cluster_name;
-      cluster_name << "cluster_" << i;
-      pcl::visualization::PointCloudColorHandlerRandom<pcl::PointXYZ> random_handler (clusters[i]);
-      vis.addPointCloud<pcl::PointXYZ> (clusters[i], random_handler, cluster_name.str ());
-
-      global.setInputCloud (xyz_points);
-      global.setIndices (indices[i].indices);
-      global.classify ();
-
-      std::vector < std::string > categories;
-      std::vector<float> conf;
-
-      global.getCategory (categories);
-      global.getConfidence (conf);
-
-      std::string category = categories[0];
-      Eigen::Vector4f centroid;
-      pcl::compute3DCentroid (*xyz_points, indices[i].indices, centroid);
-      for (size_t kk = 0; kk < categories.size (); kk++)
-      {
-
-        pcl::PointXYZ pos;
-        pos.x = centroid[0] + normal_plane_[0] * static_cast<float> (kk + 1) * dist_;
-        pos.y = centroid[1] + normal_plane_[1] * static_cast<float> (kk + 1) * dist_;
-        pos.z = centroid[2] + normal_plane_[2] * static_cast<float> (kk + 1) * dist_;
-
-        std::ostringstream prob_str;
-        prob_str.precision (1);
-        prob_str << categories[kk] << " [" << conf[kk] << "]";
-
-        std::stringstream cluster_text;
-        cluster_text << "cluster_" << previous_categories_size << "_text";
-        vis.addText3D (prob_str.str (), pos, text_scale, 1, 0, 1, cluster_text.str (), 0);
-        previous_categories_size++;
-      }
-    }
-
-    previous_cluster_size = clusters.size ();
-
-    vis.spinOnce ();
+  while (!vis.wasStopped ())
+  {
+    vis.spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
   }
 }
 
