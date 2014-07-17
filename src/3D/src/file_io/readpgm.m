3bfa6f80391e8e31854444efac1a9fd66401d600
MichaelF <mdfirman@gmail.com>
2014-07-17 18:37:48 +0100

    Bugfixes and improvements to utility functions:

--- src/3D/src/file_io/readpgm.m
+++ src/3D/src/file_io/readpgm.m
@@ -1,7 +1,18 @@
-function P = readpgm(filename)
+function depth_image = readpgm(filename)
+% Reads in a depth image from a .pgm file as outputted from blensor
+% Probably (definitely) not compatible with any other type of pgm file!
 
 fid = fopen(filename, 'r');
-P = textscan(fid,'%f','headerlines',4);
+
+% reading header
+fgetl(fid); % P2
+fgetl(fid); % Blensor output
+array_size = fscanf(fid, '%d %d\n', [1, 2]); % image size
+fgetl(fid); % 65535
+
+% reading file contents
+file_contents = textscan(fid,'%f');
+depth_image = flipud(reshape(file_contents{1}, array_size)');
+
 fclose(fid);
 
-P = flipud(reshape(P{1},[640, 480])');
--- src/3D/src/utils/centroid_2d_to_linear_index.m
+++ src/3D/src/utils/centroid_2d_to_linear_index.m
@@ -3,9 +3,9 @@ function linear_index =  centroid_2d_to_linear_index(centroid_2d, mask)
 % This index refers to the pixel on the mask closest to the 2D centroid...
 
 [XX, YY] = find(mask);
-[~, idx] = pdist2([YY, XX], centroid_2d, 'euclidean', 'smallest', 1);
+[~, linear_index] = pdist2([YY, XX], centroid_2d, 'euclidean', 'smallest', 1);
 
-temp_mask = +mask;
-temp_mask(mask) = 1:sum(sum(mask));
-
-linear_index = temp_mask(round(centroid_2d(2)), round(centroid_2d(1)));
+% error check
+if linear_index == 0 || isempty(linear_index)
+    keyboard;
+end
