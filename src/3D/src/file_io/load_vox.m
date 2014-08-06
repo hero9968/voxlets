c77ab40603478ac764365e9888a1b04a0a91a70f
MichaelF <mdfirman@gmail.com>
2014-08-06 15:43:00 +0100

    Updated load_voxels to now work on the new sparse version

--- src/3D/src/file_io/load_vox.m
+++ src/3D/src/file_io/load_vox.m
@@ -2,8 +2,13 @@ function vox = load_vox(model_name)
 
 voxel_filename = sprintf('/Users/Michael/projects/shape_sharing/data/3D/basis_models/voxelised/%s.mat', model_name);
 vox_struct = load(voxel_filename);
-V = vox_struct.vol;
-V(V<41) = 0;
+
+size_vect = double(vox_struct.res)*ones(1, 3);
+V = full_3d( size_vect, vox_struct.sparse_volume);
+
+%V = vox_struct.vol;
+%V(V<41) = 0;
+
 V = permute(V, [2, 1, 3]);
 [inds] = find(V);
 [i, j, k] = ind2sub(size(V), inds);
