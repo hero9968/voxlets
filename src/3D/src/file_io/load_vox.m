85f9d3c10306c96c92c01a750de72bd351c9f98c
MichaelF <mdfirman@gmail.com>
2014-08-08 01:19:12 +0100

    Fixed bug in loading voxels

--- src/3D/src/file_io/load_vox.m
+++ src/3D/src/file_io/load_vox.m
@@ -2,12 +2,16 @@ function vox = load_vox(model_name)
 
 voxel_filename = sprintf('/Users/Michael/projects/shape_sharing/data/3D/basis_models/voxelised/%s.mat', model_name);
 vox_struct = load(voxel_filename);
-
-T = vox_struct.sparse_volume;
-V = full_3d(vox_struct.size * [1, 1, 1], T);
-%V(V<41) = 0;
-
-V = permute(V, [2, 1, 3]);
-[inds] = find(V);
-[i, j, k] = ind2sub(size(V), inds);
+T = double(vox_struct.sparse_volume(:));
+%V = full_3d(double(vox_struct.res) * [1, 1, 1], T);
+vox_size = double(vox_struct.res) * [1, 1, 1];
+[j, i, k] = ind2sub(vox_size, T);
 vox = [i, j, k];
+
+
+
+% full find function on the 3d data returning all the indices
+function [a, b, c] = fullfind(V)
+
+[a, temp] = find(V);
+[b, c] = ind2sub([size(V, 2), size(V, 3)], temp);
