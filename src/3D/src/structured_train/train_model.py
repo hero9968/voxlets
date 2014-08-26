2b4ef2a8f6c91211e7cf1ac998d4337352c2a378
MichaelF <mdfirman@gmail.com>
2014-08-26 13:23:05 +0100

    A few additional features and niceties to the training of the model:

--- src/3D/src/structured_train/train_model.py
+++ src/3D/src/structured_train/train_model.py
@@ -1,7 +1,7 @@
 '''
 This script brings together all the structured information and trains a RF on them
 '''
-
+#import pdb; pdb.set_trace()
 import os
 import collections
 import numpy as np
@@ -10,14 +10,14 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 import cPickle as pickle
-
-patch_features_only = False
+import timeit
 
 base_path = os.path.expanduser("~/projects/shape_sharing/data/3D/basis_models/")
 models_list = base_path + 'databaseFull/fields/models.txt'
+split_path = base_path + 'structured/split.mat'
 
 def load_modeldata(modelname):
-	modelpath = base_path + 'features/' + modelname + '.mat'
+	modelpath = base_path + 'structured/features/' + modelname + '.mat'
 	if os.path.isfile(modelpath):
 		return scipy.io.loadmat(modelpath)
 	else:
@@ -46,6 +46,7 @@ def replace_nans_with_col_means(X):
 	'''
 	http://stackoverflow.com/questions/18689235/numpy-array-replace-nan-values-with-average-of-columns
 	'''
+	print X.shape
 	col_mean = stats.nanmean(X,axis=0)
 	inds = np.where(np.isnan(X))
 	X[inds]=np.take(col_mean,inds[1])
@@ -67,50 +68,69 @@ def resample_inputs(X, Y, num_samples):
 
 	return X[idxs,:], Y[idxs]
 
+# setting up the options
+number_trees = 10
+num_samples = 250000
+use_spider = False
+testing_only = False # if true, then only load a few of the models (v quick, good for testing)
 
 if __name__ == '__main__':
 
+	# seeding random numbers so that in theory each run will generate the same forest 
+	# - not really sure if this will be the case though
+	np.random.seed(1)
+
 	# loading the data
 	# For now, am only going to use one file to train on...
-	f = open(models_list, 'r')
+	#f = open(models_list, 'r')
+	train_names = scipy.io.loadmat(split_path)['train_names']
 	rawdata = []
-	for idx, line in enumerate(f):
+	print "There are " + str(len(train_names)) + " training objects"
+	for idx, line in enumerate(train_names):
 		temp = load_modeldata(line.strip())
 		if temp:
 			rawdata.append(temp)
 		print idx 
-		if idx > 75:
+		if testing_only and idx > 7:
 			break
-	f.close()
 
 	data = list_of_dicts_to_dict(rawdata)
+	print "Spider is " + str(np.array(data['spider_features']).shape)
 
 	# constructing the X and Y variables
 	Y = np.array(data['depth_diffs']).ravel()
 	Y = nan_to_value(Y, 0)
-	print Y.shape
-	patch_in = np.array(data['patch_features']).reshape((-1, data['patch_features'][0].shape[1]))
-	patch_features = nan_to_value(patch_in, 0)
-	spider_features = replace_nans_with_col_means(np.array(data['spider_features']))
-	#if patch_features_only:
+
+	print "Reshaping..."
+	patch_feature_dimension = data['patch_features'][0].shape[1]
+	patch_feature_nan = np.array(data['patch_features']).reshape(-1, patch_feature_dimension)
+
+	print "Removing nans..."
+	patch_features = nan_to_value(patch_feature_nan, 0)
+
+	if use_spider:
+		spider_feature_dimension = data['spider_features'][0].shape[1]
+		spider_features = replace_nans_with_col_means(np.array(data['spider_features']))
+		spider_features = spider_features.reshape(-1, spider_feature_dimension)
+
 	X = patch_features
-	#else:
-	#	X = np.concatenate((patch_features, spider_features), axis=1)
+	#X = np.concatenate((patch_features, spider_features), axis=1)
 
-	#import pdb; pdb.set_trace()
-
+	print "Before resampling..."
 	print "X shape is " + str(X.shape)
 	print "Y shape is " + str(Y.shape)
 
 	# Here do some subsampling to reduce the size of the input datasets...
-	num_samples = 250#000
-	X,Y = resample_inputs(X, Y, num_samples)
+	X, Y = resample_inputs(X, Y, num_samples)
 
+	print "After resampling..."
 	print "X shape is " + str(X.shape)
 	print "Y shape is " + str(Y.shape)
 
 	# training the forest
 	print "Training forest..."
-	clf = RandomForestRegressor(n_estimators=10,n_jobs=4)
+	tic = timeit.default_timer()
+	clf = RandomForestRegressor(n_estimators=number_trees,n_jobs=-1)
 	clf.fit(X,Y)
-	pickle.dump(clf, open("spidermodel.pkl", "wb") )
+	pickle.dump(clf, open("spidermodel2.pkl", "wb") )
+	print 'Done training in ' + str(timeit.default_timer() - tic)
