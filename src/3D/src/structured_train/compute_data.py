MAJOR BUGFIX in the feature computation - sampling indices. Also some minor bugfixes and features added

src/3D/src/structured_train/compute_data.py
@@ -94,26 +94,28 @@ class depth_feature_engine(object):
 
 		#border size is the width at the edge of the mask from which we are not allowed to sample
 		self.indices = np.array(np.nonzero(self.mask))
+		
 		if self.indices.shape[1]:
 			scipy.io.savemat('mask.mat', dict(mask=self.mask))
 
 		# removing indices which are too near the border
 		# Should use np.logical_or.reduce(...) instead of this...
+		#print self.indices
 		to_remove = np.logical_or(np.logical_or(self.indices[0] < border_size, 
 												self.indices[1] < border_size),
 								np.logical_or(self.indices[0] > self.mask.shape[0]-border_size,
 												self.indices[1] > self.mask.shape[1]-border_size))
 		self.indices = np.delete(self.indices, np.nonzero(to_remove), 1).transpose()
 
-	
 		# doing the sampling
+		#print self.indices
 		if self.samples_per_image == -1:
 			pass # do nothing - keep all indices
 		elif np.any(self.indices): # downsample indices
-			self.indices = self.indices[np.random.randint(0, self.indices.shape[1], self.samples_per_image), :]
+			samples = np.random.randint(0, self.indices.shape[0], self.samples_per_image)
+			self.indices = self.indices[samples, :]
 		else: # no indices - just repeat centre point
 			self.indices = np.tile(np.array(self.mask.shape)/2, (samples_per_image, 1))
-			
 		return self.indices
 
 	def depth_difference(self, index):
@@ -207,9 +209,9 @@ class depth_feature_engine(object):
 		'''
 		plot the front render and the sampled pairs
 		'''
-		plt.imshow(frontrender)
+		plt.imshow(self.frontrender)
 		plt.hold(True)
-		plt.plot(indices[:, 1], indices[:, 0], '.')
+		plt.plot(self.indices[:, 1], self.indices[:, 0], '.')
 		plt.hold(False)
 		if filename:
 			plt.savefig(filename)
@@ -299,6 +301,7 @@ if __name__ == '__main__':
 		scipy.io.savemat(fileout, fulldict)
 
 		print 'Done ' + str(idx) + ' in ' + str(timeit.default_timer() - tic)
+		#break
 		
 	f.close()
 

src/3D/src/structured_train/sparse_evaluation.py
@@ -5,6 +5,7 @@ This script just loads them and plots them
 '''
 
 import yaml
+import numpy as np
 import scipy.io
 import matplotlib.pyplot as pl
 import os.path
@@ -25,9 +26,17 @@ for modeloption in all_models:
 		print "Skipping " + result_path
 
 print "In total there are : " + str(len(predictions))
+
+# sorting by their auc
+all_auc = [pred['roc_auc'] for pred in predictions]
+print "AUC = " + str(np.array(all_auc).flatten())
+sort_index = np.argsort(np.array(all_auc).flatten())
+print "Sorted = " + str(sort_index)
+
 # Plot ROC curve for the models
 pl.clf()
-for pred in predictions:
+for idx in sort_index[::-1]:
+	pred = predictions[idx]
 	label = pred['name'] + ' (area = %0.2f)' % pred['roc_auc']
 	pl.plot(pred['fpr'].flatten(), pred['tpr'].flatten(), label=label)
 	
@@ -36,7 +45,6 @@ pl.xlim([0.0, 1.0])
 pl.ylim([0.0, 1.0])
 pl.xlabel('False Positive Rate')
 pl.ylabel('True Positive Rate')
-pl.title('Receiver operating characteristic example')
 pl.legend(loc="lower right") 
 #pl.show()
 pl.savefig('plots/roc.eps')