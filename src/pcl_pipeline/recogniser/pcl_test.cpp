4ac4b8f2cf8f1e4600ff5d41891c62c515abd4bb
MichaelF <mdfirman@gmail.com>
2014-10-13 13:56:22 +0100

    Removed a load of shit

--- src/pcl_pipeline/recogniser/pcl_test.cpp
+++
@@ -1,9 +0,0 @@
-//#include <iostream>
-#include <pcl/io/pcd_io.h>
-
-int main()
-{
-	pcl::Histogram<190> crh_histogram; //(new pcl::Histogram<190>());
-	//pcl::PointCloud<pcl::PointXYZ >::Ptr crh_histogram (new pcl::PointCloud<pcl::PointXYZ >());
-	pcl::io::savePCDFileASCII ("test_file", crh_histogram);
-}
