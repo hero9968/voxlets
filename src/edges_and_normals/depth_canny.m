527e4bb85b3a9d007d2343b6a33a5a7413dee69c
MichaelF <mdfirman@gmail.com>
2014-10-15 00:14:08 +0100

    Canny edge detection looking ok now. Needs cleaning up and making much better, removing a lot of chaff...

---
+++ src/edges_and_normals/depth_canny.m
@@ -0,0 +1,237 @@
+function [eout,thresh] = depth_canny(magGrad, depth_im)
+% EDGE Find edges in intensity image.
+% 
+% 
+
+%[a,method,thresh,sigma,thinning,H,kx,ky] = parse_inputs(varargin{:});
+
+[m,n] = size(magGrad);
+
+% The output edge map:
+e = false(m,n);
+
+%if strcmp(method,'canny')
+    % Magic numbers
+    PercentOfPixelsNotEdges = .9; % Used for selecting thresholds
+    ThresholdRatio = 0.3;          % Low thresh is this fraction of the high.
+    
+    % Calculate gradients using a derivative of Gaussian filter
+    sigma = sqrt(2);
+    [dx, dy] = smoothGradient(depth_im, sigma);
+    
+    % Calculate Magnitude of Gradient
+    %magGrad = hypot(dx, dy);
+    
+    % Normalize for threshold selection
+    magmax = max(magGrad(:));
+    if magmax > 0
+        magGrad = magGrad / magmax;
+    end
+    
+    % Determine Hysteresis Thresholds
+    [lowThresh, highThresh] = selectThresholds([], magGrad, PercentOfPixelsNotEdges, ThresholdRatio, mfilename);
+    
+    % Perform Non-Maximum Suppression Thining and Hysteresis Thresholding of Edge
+    % Strength
+    e = thinAndThreshold(e, dx, dy, magGrad, lowThresh, highThresh);
+    thresh = [lowThresh highThresh];
+    
+%end
+
+if nargout==0,
+    imshow(e);
+else
+    eout = e;
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Local Function : computeEdgesWithThinning
+%
+function e = computeEdgesWithThinning(b,bx,by,kx,ky,offset,cutoff)
+% This subfunction computes edges using edge thinning.
+
+bx = abs(bx); % necessary for doing local maxima suppression
+by = abs(by);
+
+e = computeedge(b,bx,by,kx,ky,int8(offset),100*eps,cutoff);
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Local Function : cannyFindLocalMaxima
+%
+function idxLocalMax = cannyFindLocalMaxima(direction,ix,iy,mag)
+%
+% This sub-function helps with the non-maximum suppression in the Canny
+% edge detector.  The input parameters are:
+%
+%   direction - the index of which direction the gradient is pointing,
+%               read from the diagram below. direction is 1, 2, 3, or 4.
+%   ix        - input image filtered by derivative of gaussian along x
+%   iy        - input image filtered by derivative of gaussian along y
+%   mag       - the gradient magnitude image
+%
+%    there are 4 cases:
+%
+%                         The X marks the pixel in question, and each
+%         3     2         of the quadrants for the gradient vector
+%       O----0----0       fall into two cases, divided by the 45
+%     4 |         | 1     degree line.  In one case the gradient
+%       |         |       vector is more horizontal, and in the other
+%       O    X    O       it is more vertical.  There are eight
+%       |         |       divisions, but for the non-maximum suppression
+%    (1)|         |(4)    we are only worried about 4 of them since we
+%       O----O----O       use symmetric points about the center pixel.
+%        (2)   (3)
+
+
+[m,n] = size(mag);
+
+% Find the indices of all points whose gradient (specified by the
+% vector (ix,iy)) is going in the direction we're looking at.
+
+switch direction
+    case 1
+        idx = find((iy<=0 & ix>-iy)  | (iy>=0 & ix<-iy));
+    case 2
+        idx = find((ix>0 & -iy>=ix)  | (ix<0 & -iy<=ix));
+    case 3
+        idx = find((ix<=0 & ix>iy) | (ix>=0 & ix<iy));
+    case 4
+        idx = find((iy<0 & ix<=iy) | (iy>0 & ix>=iy));
+end
+
+% Exclude the exterior pixels
+if ~isempty(idx)
+    v = mod(idx,m);
+    extIdx = (v==1 | v==0 | idx<=m | (idx>(n-1)*m));
+    idx(extIdx) = [];
+end
+
+ixv = ix(idx);
+iyv = iy(idx);
+gradmag = mag(idx);
+
+% Do the linear interpolations for the interior pixels
+switch direction
+    case 1
+        d = abs(iyv./ixv);
+        gradmag1 = mag(idx+m).*(1-d) + mag(idx+m-1).*d;
+        gradmag2 = mag(idx-m).*(1-d) + mag(idx-m+1).*d;
+    case 2
+        d = abs(ixv./iyv);
+        gradmag1 = mag(idx-1).*(1-d) + mag(idx+m-1).*d;
+        gradmag2 = mag(idx+1).*(1-d) + mag(idx-m+1).*d;
+    case 3
+        d = abs(ixv./iyv);
+        gradmag1 = mag(idx-1).*(1-d) + mag(idx-m-1).*d;
+        gradmag2 = mag(idx+1).*(1-d) + mag(idx+m+1).*d;
+    case 4
+        d = abs(iyv./ixv);
+        gradmag1 = mag(idx-m).*(1-d) + mag(idx-m-1).*d;
+        gradmag2 = mag(idx+m).*(1-d) + mag(idx+m+1).*d;
+end
+idxLocalMax = idx(gradmag>=gradmag1 & gradmag>=gradmag2);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Local Function : smoothGradient
+%
+function [GX, GY] = smoothGradient(I, sigma)
+
+% Create an even-length 1-D separable Derivative of Gaussian filter
+
+% Determine filter length
+filterLength = 8*ceil(sigma);
+n = (filterLength - 1)/2;
+x = -n:n;
+
+% Create 1-D Gaussian Kernel
+c = 1/(sqrt(2*pi)*sigma);
+gaussKernel = c * exp(-(x.^2)/(2*sigma^2));
+
+% Normalize to ensure kernel sums to one
+gaussKernel = gaussKernel/sum(gaussKernel);
+
+% Create 1-D Derivative of Gaussian Kernel
+derivGaussKernel = gradient(gaussKernel);
+
+% Normalize to ensure kernel sums to zero
+negVals = derivGaussKernel < 0;
+posVals = derivGaussKernel > 0;
+derivGaussKernel(posVals) = derivGaussKernel(posVals)/sum(derivGaussKernel(posVals));
+derivGaussKernel(negVals) = derivGaussKernel(negVals)/abs(sum(derivGaussKernel(negVals)));
+
+% Compute smoothed numerical gradient of image I along x (horizontal)
+% direction. GX corresponds to dG/dx, where G is the Gaussian Smoothed
+% version of image I.
+GX = imfilter(I, gaussKernel', 'conv', 'replicate');
+GX = imfilter(GX, derivGaussKernel, 'conv', 'replicate');
+
+% Compute smoothed numerical gradient of image I along y (vertical)
+% direction. GY corresponds to dG/dy, where G is the Gaussian Smoothed
+% version of image I.
+GY = imfilter(I, gaussKernel, 'conv', 'replicate');
+GY  = imfilter(GY, derivGaussKernel', 'conv', 'replicate');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Local Function : selectThresholds
+%
+function [lowThresh, highThresh] = selectThresholds(thresh, magGrad, PercentOfPixelsNotEdges, ThresholdRatio, ~)
+
+[m,n] = size(magGrad);
+
+% Select the thresholds
+if isempty(thresh)
+    counts=imhist(magGrad, 64);
+    highThresh = find(cumsum(counts) > PercentOfPixelsNotEdges*m*n,...
+        1,'first') / 64;
+    lowThresh = ThresholdRatio*highThresh;
+elseif length(thresh)==1
+    highThresh = thresh;
+    if thresh>=1
+        error(message('images:edge:thresholdMustBeLessThanOne'))
+    end
+    lowThresh = ThresholdRatio*thresh;
+elseif length(thresh)==2
+    lowThresh = thresh(1);
+    highThresh = thresh(2);
+    if (lowThresh >= highThresh) || (highThresh >= 1)
+        error(message('images:edge:thresholdOutOfRange'))
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   Local Function : thinAndThreshold
+%
+function H = thinAndThreshold(E, dx, dy, magGrad, lowThresh, highThresh)
+
+% Perform Non-Maximum Suppression Thining and Hysteresis Thresholding of Edge
+% Strength
+    
+% We will accrue indices which specify ON pixels in strong edgemap
+% The array e will become the weak edge map.
+idxStrong = [];
+for dir = 1:4
+    idxLocalMax = cannyFindLocalMaxima(dir,dx,dy,magGrad);
+    idxWeak = idxLocalMax(magGrad(idxLocalMax) > lowThresh);
+    E(idxWeak)=1;
+    idxStrong = [idxStrong; idxWeak(magGrad(idxWeak) > highThresh)]; %#ok<AGROW>
+end
+
+[m,n] = size(E);
+
+if ~isempty(idxStrong) % result is all zeros if idxStrong is empty
+    rstrong = rem(idxStrong-1, m)+1;
+    cstrong = floor((idxStrong-1)/m)+1;
+    H = bwselect(E, cstrong, rstrong, 8);
+else
+    H = zeros(m, n);
+end
---
+++ src/edges_and_normals/edge_detect.m
@@ -0,0 +1,75 @@
+addpath('/Users/Michael/builds/research_code/structured_edge_detection/release')
+addpath(genpath('/Users/Michael/projects/shape_sharing/src/matlab/toolbox'))
+load('./data/modelNyuRgbd.mat')
+
+%%
+stack = cat(3, 0.01*im2double(bb_cropped.rgb), bb_cropped.depth);
+structured_edge = edgesDetect(stack, model);
+imagesc(structured_edge)
+set(gca, 'clim', [0, 1])
+axis image
+
+%%
+canny_edge = depth_canny(structured_edge, bb_cropped.depth);
+
+%%
+%close all
+subplot(131); imagesc(bb_cropped.depth); axis image
+subplot(132); imagesc(structured_edge); colorbar; axis image
+subplot(133); imshow(repmat(uint8(canny_edge*255), [1, 1, 3])); axis image
+colormap(jet)
+
+%%
+image_path = '/Volumes/HDD/data/others_data/RGBD_datasets/rgbd_scenes2//rgbd-scenes-v2/imgs/scene_01/';
+image_name = '00401';
+rgb_path = [image_path, image_name, '-color.png'];
+depth_path = [image_path, image_name, '-depth.png'];
+
+addpath('~/projects/shape_sharing/src/preprocess/bigbird/crop_smooth/')
+
+C.rgb = imread(rgb_path);
+C.gray = im2double(rgb2gray(C.rgb));
+C.depth = double(imread(depth_path))/10000;
+C.all = cat(3, 0.5*C.rgb, C.depth);
+C.edges = edgesDetect(C.all, model);
+C.smoothed_depth = fill_depth_colorization_ft_mex(C.gray, C.depth);
+
+%%
+stack = cat(3, 0.1*im2double(C.rgb), C.smoothed_depth);
+structured_edge = edgesDetect(stack, model);
+%structured_edge = structured_edge / max(structured_edge(:));
+canny_edge = depth_canny(structured_edge, C.smoothed_depth);
+
+
+H1 = subplot(222); imagesc(C.smoothed_depth);  axis image
+subplot(221); imagesc(C.depth); axis image; set(gca, 'clim', get(H1, 'clim'))
+subplot(223); imagesc(structured_edge);  axis image
+subplot(224); imshow(repmat(uint8(canny_edge*255), [1, 1, 3])); axis image
+colormap(jet)
+%%
+close all
+imshow(repmat(uint8(canny_edge*255), [1, 1, 3])); axis image
+
+%%
+get(H1)
+% turntable objects to start with
+
+% smooth output
+
+% kinectfusion on sum3d? 
+
+% smoothing on depth image?? Bilateral?
+
+
+% Fast MRF Optimization with Application to Depth Reconstruction
+
+% canny edge detetor - non-max suppression
+% find local peak in 
+
+% graph cuts - remove spurious edges
+% slightly fat depth edge, should reinforce where teh gradients are in the
+% image. Use internsity image as well 
+
+% spider features for different thresholds
+
+% do on isolated objects to start with 
