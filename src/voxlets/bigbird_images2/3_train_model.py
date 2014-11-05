ff6efd6ebb75fec45c7f8a47bd432bfd8b4b3148
MichaelF <mdfirman@gmail.com>
2014-11-05 01:34:34 +0000

    Fixed a shit load of bugs in the model training. Ready for troll I think

--- src/voxlets/bigbird_images2/3_train_model.py
+++ src/voxlets/bigbird_images2/3_train_model.py
@@ -5,13 +5,13 @@ import numpy as np
 import cPickle as pickle
 import scipy.io
 import sys
-sys.path.append('/Users/Michael/projects/shape_sharing/src/')
+sys.path.append(os.path.expanduser('~/projects/shape_sharing/src/'))
 from sklearn.ensemble import RandomForestClassifier
 
 from common import paths
 
 "Parameters"
-max_data_in_subsample = 300000
+max_data_in_subsample = 1000000
 number_trees = 100
 if paths.host_name != 'troll':
     small_sample = True
@@ -20,11 +20,15 @@ else:
 max_depth = 15
 if small_sample: print "WARNING: Just computing on a small sample"
 
+####################################################################
+print "Loading the dictionary"
+km = pickle.load(open(paths.voxlet_dict_path, 'rb'))
+
 
 ####################################################################
 print "Loading in all the data..."
-shoeboxes = []
 features = []
+all_idxs = []
 for count, modelname in enumerate(paths.train_names):
 
     # loading the data
@@ -32,47 +36,44 @@ for count, modelname in enumerate(paths.train_names):
     print "Loading from " + loadpath
 
     D = scipy.io.loadmat(loadpath)
-    shoeboxes.append(D['shoeboxes'])
 
     features.append(D['features'])
     
+    print "Assigning model shoeboxes to clusters"
+    idx_assign = km.predict(D['shoeboxes'].astype(np.float32))
+
+    all_idxs.append(idx_assign)
+
+
     if count > 2 and small_sample:
         print "SMALL SAMPLE: Stopping"
         break
 
-np_all_sboxes = np.concatenate(shoeboxes, axis=0).astype(np.float16)
-
-
-####################################################################
-print "Loading the dictionary"
-km = pickle.load(open(paths.voxlet_dict_path, 'rb'))
-
-
-####################################################################
-print "Assigning shoeboxes to clusters"
-idx_assign = km.predict(np_all_sboxes)
-
+np_all_idxs = np.hstack(all_idxs)
 
 ####################################################################
 print "Now training the forest"
+np_features = np.array(features).reshape((-1, 56))
 
-np_features = np.concatenate(features, axis=0)
-print "Idx assign has shape " + str(idx_assign.shape)
+print "Idx assign has shape " + str(np_all_idxs.shape)
 
 if max_data_in_subsample > np_features.shape[0]:
     print "Using all data..."
     np_features_subset = np_features
+    np_all_idxs_subset = np_all_idxs
 else:
     print "Subsampling data..."
     to_use_for_clustering = np.random.randint(0, np_features.shape[0], size=(max_data_in_subsample))
     np_features_subset = np_features[to_use_for_clustering, :]
-    idx_assign = idx_assign[to_use_for_clustering]
+    np_all_idxs_subset = np_all_idxs[to_use_for_clustering]
 
 print "Features shape before subsampling :" + str(np_features.shape)
 print "Features shape after subsampling :" + str(np_features_subset.shape)
+print "Idxs shape after subsampling :" + str(np_all_idxs.shape)
+print "Idxs shape after subsampling :" + str(np_all_idxs_subset.shape)
 
-forest = RandomForestClassifier(n_estimators=number_trees, criterion="entropy", oob_score=True, max_depth=max_depth)
-forest.fit(np_features_subset, idx_assign)
+forest = RandomForestClassifier(n_estimators=number_trees, criterion="entropy", oob_score=True, max_depth=max_depth, n_jobs=8)
+forest.fit(np_features_subset, np_all_idxs_subset)
 
 print "Done training, now saving"
 pickle.dump(forest, open(paths.voxlet_model_path, 'wb'))
