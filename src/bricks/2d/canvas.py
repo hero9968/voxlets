03ee3d8c0510aefd943f9d1a89a77ee4af05f086
MichaelF <mdfirman@gmail.com>
2015-02-10 16:20:04 +0000

    ipynb for doing more of this

--- src/bricks/2d/canvas.py
+++ src/bricks/2d/canvas.py
@@ -4,12 +4,13 @@ functions for dealing with 2d canvases
 import numpy as np
 import scipy
 from scipy.ndimage.morphology import distance_transform_edt
+from sklearn.neighbors import NearestNeighbors
 
 # Set some parameters here
-box_size = (20, 30)
-circle_size = (30, 30)
-canvas_size = (100, 100)
-
+box_size = (5, 25)
+circle_size = (15, 15)
+canvas_size = (50, 50)
+pad_region = 20
 
 class Canvas(object):
     def __init__(self):
@@ -22,39 +23,52 @@ class Canvas(object):
         return scipy.misc.imrotate(
             box, np.random.rand() * 360, interp='nearest').astype(np.float32) / 255
 
-
-    def random_fill(self):
+    def random_fill(self, num_boxes=1, num_circles=1):
         '''
         fill with random scene
         '''
-        circle = 255 - scipy.misc.imread('data/circle.png')
-        circle = scipy.misc.imresize(
-            circle, circle_size, interp='nearest').astype(np.float32) / 255
+        max_iters = 1000
+        count = 0
 
-        box = self._random_rotated_box()
+        while True:
+            circle = 255 - scipy.misc.imread('data/circle.png')
+            circle = scipy.misc.imresize(
+                circle, circle_size, interp='nearest').astype(np.float32) / 255
 
-        canvas = np.zeros(canvas_size)
+            canvas = np.zeros(canvas_size)
 
-        box_loc = (np.random.randint(0, canvas.shape[0] - box.shape[0]),
-                   np.random.randint(0, canvas.shape[1] - box.shape[1]))
+            for i in range(num_boxes):
+                box = self._random_rotated_box()
 
-        circle_loc = (np.random.randint(0, canvas.shape[0] - circle.shape[0]),
-                      np.random.randint(0, canvas.shape[1] - circle.shape[1]))
+                box_loc = (np.random.randint(0, canvas.shape[0] - box.shape[0]),
+                           np.random.randint(0, canvas.shape[1] - box.shape[1]))
 
-        canvas[box_loc[0]:(box_loc[0] + box.shape[0]),
-               box_loc[1]:(box_loc[1] + box.shape[1])] += box
-        canvas[circle_loc[0]:(circle_loc[0] + circle.shape[0]),
-               circle_loc[1]:(circle_loc[1] + circle.shape[1])] += circle
+                canvas[box_loc[0]:(box_loc[0] + box.shape[0]),
+                       box_loc[1]:(box_loc[1] + box.shape[1])] += box
 
+            for i in range(num_circles):
+                circle_loc = (np.random.randint(0, canvas.shape[0] - circle.shape[0]),
+                              np.random.randint(0, canvas.shape[1] - circle.shape[1]))
+                canvas[circle_loc[0]:(circle_loc[0] + circle.shape[0]),
+                       circle_loc[1]:(circle_loc[1] + circle.shape[1])] += circle
+
+            if np.max(canvas) < 1.0001:
+                break
+            elif count > max_iters:
+                raise Exception("Couldn't generate a canvas")
+            count += 1
+
+        print count
         self.im = canvas > 0.5
-
+        self.im = np.pad(
+            self.im, [[pad_region, pad_region], [pad_region, pad_region]],
+            'constant', constant_values=0)
 
     def render_canvas(self):
         '''renders canvas from the top down'''
         pix_in_col =  np.array([np.where(col==1)[0] for col in self.im.T])
         self.depth_image = \
             [pix[0] if pix.shape[0] > 0 else self.im.shape[0] for pix in pix_in_col]
-
 
     # maybe now do a tsdf on this?
     def tsdf_from_render(self, mu):
@@ -74,7 +88,6 @@ class Canvas(object):
 
         return self.render_tsdf
 
-
     def full_tsdf(self, mu):
 
         trans_inside = distance_transform_edt(self.im)
@@ -85,12 +98,116 @@ class Canvas(object):
         sdf[sdf > mu] = mu
         sdf[sdf < -mu] = -mu
 
-        return sdf
+        self.sdf = sdf
+        return self.sdf
 
     @classmethod
-    def fill_and_render(cls):
+    def fill_and_render(cls, num_boxes=1, num_circles=1):
         im = cls()
-        im.random_fill()
+        im.random_fill(num_boxes, num_circles)
         im.render_canvas()
         im.tsdf_from_render(10)
+        im.full_tsdf(10)
         return im
+
+    def extract_windows(self, sliding_window_shape):
+
+        potential_places = np.array(
+            [(point, idx)
+            for idx, point in enumerate(self.depth_image)
+            if point < self.im.shape[0]])
+
+        all_X, all_Y = [], []
+
+        for place in potential_places:
+
+            top = place[0] - sliding_window_shape[0] / 2
+            bottom = place[0] + sliding_window_shape[0] / 2
+            left = place[1] - sliding_window_shape[1] / 2
+            right = place[1] + sliding_window_shape[1] / 2
+
+            Y = self.im[top:bottom, left:right]
+            X = self.render_tsdf[top:bottom, left:right]
+
+            all_X.append(X.flatten())
+            all_Y.append(Y.flatten())
+
+        all_X_np = np.vstack(all_X)
+        all_Y_np = np.vstack(all_Y)
+
+        return all_X_np, all_Y_np
+
+
+    def predict_from_windows(
+            self, sliding_window_shape, train_X, train_Y, patch_in_known=True):
+        '''
+        uses train_X and train_Y to try to fill in the missing parts!
+        probably wont work too well but we'll see...
+        '''
+
+        # follwing line should perhaps not be here...
+        nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(train_X)
+
+        potential_places = np.array(
+            [(point, idx)
+            for idx, point in enumerate(self.depth_image)
+            if point < self.im.shape[0]])
+
+        prediction = np.zeros(self.im.shape)
+
+        for place in potential_places:
+
+            top = place[0] - sliding_window_shape[0] / 2
+            bottom = place[0] + sliding_window_shape[0] / 2
+            left = place[1] - sliding_window_shape[1] / 2
+            right = place[1] + sliding_window_shape[1] / 2
+
+            Y = self.im[top:bottom, left:right]
+            X = self.render_tsdf[top:bottom, left:right]
+
+            # finding a match for the X...
+            distances, indices = nbrs.kneighbors(X.flatten())
+
+            # patching the prediction into the prediction image...
+            # Using the gt:
+            #prediction[top:bottom, left:right] = Y
+            # Using the machine learning:
+            try:
+                prediction[top:bottom, left:right] += \
+                    train_Y[indices[0][0]].reshape(sliding_window_shape)
+            except:
+                print train_Y.shape
+                print train_Y[indices[0][0]].shape
+                print sliding_window_shape
+
+            #print indices
+
+        if patch_in_known:
+            prediction[self.render_tsdf == self.render_tsdf.max()] = 0
+            #prediction[self.render_tsdf < 0] = 1
+
+        return prediction
+
+
+
+# '''
+# now extract sliding windows from all over the rendered tsdf
+# want to then be able to say for any sliding window location input,
+# to detect ('fire') where we see a good example of something we can predict for
+# i.e. close to a training example
+# '''
+# sliding_window_shape = (20, 20)
+# canvas = training_canvases[0]
+# total = 0
+
+# # Dense method
+# training_windows = []
+# for row_idx in range(canvas.shape[0] - sliding_window_shape[0]):
+#     for col_idx in range(canvas.shape[1] - sliding_window_shape[1]):
+#         temp = canvas[row_idx:row_idx + sliding_window_shape[0],
+#                       col_idx:col_idx + sliding_window_shape[1]]
+#         if np.any(temp):
+#             training_windows.append(temp)
+#         total += 1
+
+# print total, len(training_windows)
